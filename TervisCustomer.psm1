ddb567bb2d90ac2b21a5ba823c26f05485ad5b37
ChrisMagnuson <hourglasssand@gmail.com>
2018-06-14 08:49:35 -0400

    Add dependency, move function to TervisUniversalDashboard

--- TervisCustomer.psm1
+++ TervisCustomer.psm1
@@ -286,83 +286,6 @@ function Remove-StringSpecialCharacter
 	} #PROCESS
 }
 
-function New-TervisUDPreloader {
-    [CmdletBinding(DefaultParameterSetName = "indeterminate")]
-    param(
-        [Parameter(ParameterSetName = "determinate")]
-        [ValidateRange(0, 100)]
-        $PercentComplete,
-        [Parameter(ParameterSetName = "determinate")]
-        [Parameter(ParameterSetName = "indeterminate")]
-        [PowerShellProTools.UniversalDashboard.Models.DashboardColor]$BackgroundColor,
-        [Parameter(ParameterSetName = "determinate")]
-        [Parameter(ParameterSetName = "indeterminate")]
-        [PowerShellProTools.UniversalDashboard.Models.DashboardColor]$ProgressColor,
-        [Parameter(ParameterSetName = 'circular')]
-        [Switch]$Circular,
-        [Parameter(ParameterSetName = 'circular')]
-        [ValidateSet('blue', 'red', 'green')]
-        [string]$Color,
-        [Parameter(ParameterSetName = 'circular')]
-        [ValidateSet('small', 'medium', 'large')]
-        [string]$Size
-        )
-
-    if ($PSCmdlet.ParameterSetName -eq 'circular') {
-        $sizeClass = ''
-        switch ($Size) {
-            "small" { $sizeClass = 'small' }
-            "large" { $sizeClass = 'big' }
-        }
-
-        New-UDElement -Tag 'div' -Id preloader -Attributes @{className = "preloader-wrapper $sizeClass active"} -Content {
-            New-UDElement -Tag 'div' -Attributes @{ className = "spinner-layer spinner-$color-only"} -Content {
-                New-UDElement -Tag 'div' -Attributes @{ className = 'circle-clipper left'} -Content {
-                    New-UDElement -Tag 'div' -Attributes @{ className ='circle' } 
-                }
-                New-UDElement -Tag 'div' -Attributes @{ className = 'gap-patch'} -Content {
-                    New-UDElement -Tag 'div' -Attributes @{ className ='circle' } 
-                }
-                New-UDElement -Tag 'div' -Attributes @{ className = 'circle-clipper right'} -Content {
-                    New-UDElement -Tag 'div' -Attributes @{ className ='circle' } 
-                }
-            }
-        }
-    }
-    else 
-    {
-        $OutsideAttributes = @{
-            className = "progress"
-        }
-    
-        if ($PSBoundParameters.ContainsKey("BackgroundColor")) {
-            $OutsideAttributes.style = @{
-                backgroundColor = $BackgroundColor.HtmlColor
-            }
-        }
-        
-        New-UDElement -Tag "div" -Attributes $OutsideAttributes -Content {
-            $Attributes = @{
-                className = $PSCmdlet.ParameterSetName
-            }
-    
-            if ($PSBoundParameters.ContainsKey("ProgressColor")) {
-                $Attributes.style = @{
-                    backgroundColor = $ProgressColor.HtmlColor
-                }
-            }
-    
-            if ($PSCmdlet.ParameterSetName -eq "determinate") {
-                $Attributes["style"] = @{
-                    width = "$($PercentComplete)%"
-                }
-            }
-    
-            New-UDElement -Tag "div" -Id preloader -Attributes $Attributes
-        }
-    }
-}
-
 function Install-TervisCustomer {
 	param (
 		$ComputerName
@@ -373,7 +296,8 @@ function Install-TervisCustomer {
 		TervisMicrosoft.PowerShell.Utility,
 		TervisOracleE-BusinessSuitePowerShell,
 		TervisPasswordstate,
-		TervisGithub -PowerShellGalleryDependencies UniversalDashboard, powershell-yaml -NugetDependencies @{
+		TervisGithub,
+		TervisUniversalDashboard -PowerShellGalleryDependencies UniversalDashboard, powershell-yaml -NugetDependencies @{
 			"libphonenumber-csharp" = @{
 				DependencyType = "PSGalleryNuget"
 				Source = "https://www.nuget.org/api/v2"
