612eb82a3b48556717d19c53c3f2aaa0cb668ef6
Chris Magnuson <hourglasssand@gmail.com>
2019-04-10 15:45:57 -0400

    Refactor to be more DRY

--- TervisCustomer.psm1
+++ TervisCustomer.psm1
@@ -317,94 +317,60 @@ function Remove-StringSpecialCharacter
 function Install-TervisCustomer {
 	param (
 		$ComputerName,
-		$EnvironmentName
+		$EnvironmentName,
+		$AsContainer
 	)
 	$PasswordstateAPIKey = Get-TervisPasswordstatePassword -Guid "2a45b43c-b4d1-49c8-879a-c708716c9c7b" |
-    Select-Object -ExpandProperty Password
-
-# 	Install-PowerShellApplicationUniversalDashboard @PSBoundParameters -ModuleName TervisCustomer -TervisModuleDependencies InvokeSQL,
-# 		OracleE-BusinessSuitePowerShell,
-# 		PasswordstatePowerShell,
-# 		TervisMicrosoft.PowerShell.Utility,
-# 		TervisOracleE-BusinessSuitePowerShell,
-# 		TervisPasswordstatePowerShell,
-# 		TervisGithub,
-# 		TervisUniversalDashboard,
-# 		WebServicesPowerShellProxyBuilder,
-# 		TervisMicrosoft.PowerShell.Security,
-# 		TervisCustomer -PowerShellGalleryDependencies @{
-# 			Name = "powershell-yaml"
-# 		}, 
-# 		@{
-# 				Name = "UniversalDashboard"
-# 				RequiredVersion="2.2.1"
-# 		} -PowerShellNugetDependencies @{
-# 			Name = "Oracle.ManagedDataAccess.Core"
-# 		},
-# 		@{
-# 			Name = "libphonenumber-csharp"
-# 			SkipDependencies = $true
-# 		} -CommandString @"
-# # Hopefully the below is not still needed, if it is we will have to figure out a way to import the correct directory that contains UD
-# #`$CacheDrive = Get-PSDrive -Name Cache -ErrorAction SilentlyContinue
-# #if (-not `$CacheDrive) {
-# #	Import-Module UniversalDashboard
-# #}
-
-# if (-Not `$Cache:EBSPowershellConfiguration ) {
-# 	`$Cache:EBSPowershellConfiguration = Get-TervisEBSPowershellConfiguration -Name $EnvironmentName
-# }
-
-# Set-EBSPowershellConfiguration -Configuration `$Cache:EBSPowershellConfiguration
-# Invoke-TervisCustomerSearchDashboard
-# "@ -UseTLS -DashboardPasswordstateAPIKey $PasswordstateAPIKey -Port 10000
-
-Invoke-PowerShellApplicationDockerBuild
-
-Install-PowerShellApplicationUniversalDashboard @PSBoundParameters `
-	-PasswordstateAPIKey $PasswordstateAPIKey `
-	-ModuleName TervisCustomer `
-	-TervisModuleDependencies InvokeSQL,
-		OracleE-BusinessSuitePowerShell,
-		PasswordstatePowerShell,
-		TervisMicrosoft.PowerShell.Utility,
-		TervisOracleE-BusinessSuitePowerShell,
-		TervisPasswordstatePowerShell,
-		TervisGithub,
-		TervisUniversalDashboard,
-		WebServicesPowerShellProxyBuilder,
-		TervisMicrosoft.PowerShell.Security,
-		TervisCustomer `
-	-PowerShellGalleryDependencies @{
-			Name = "powershell-yaml"
-		}, 
-		@{
+	Select-Object -ExpandProperty Password
+	
+	$Parameters = @{
+		ComputerName = $ComputerName
+		EnvironmentName = $EnvironmentName
+		PasswordstateAPIKey = $PasswordstateAPIKey
+		ModuleName = "TervisCustomer"
+		TervisModuleDependencies = "InvokeSQL",
+			"OracleE-BusinessSuitePowerShell",
+			"PasswordstatePowerShell",
+			"TervisMicrosoft.PowerShell.Utility",
+			"TervisOracleE-BusinessSuitePowerShell",
+			"TervisPasswordstatePowerShell",
+			"TervisGithub",
+			"TervisUniversalDashboard",
+			"WebServicesPowerShellProxyBuilder",
+			"TervisMicrosoft.PowerShell.Security",
+			"TervisCustomer"
+		PowerShellGalleryDependencies = @{
+				Name = "powershell-yaml"
+			}, 
+			@{
 				Name = "UniversalDashboard"
-				RequiredVersion="2.2.1"
-		} -PowerShellNugetDependencies @{
-			Name = "Oracle.ManagedDataAccess.Core"
-		},
-		@{
-			Name = "libphonenumber-csharp"
-			SkipDependencies = $true
-		} `
-	-CommandString @"
-# Hopefully the below is not still needed, if it is we will have to figure out a way to import the correct directory that contains UD
-#`$CacheDrive = Get-PSDrive -Name Cache -ErrorAction SilentlyContinue
-#if (-not `$CacheDrive) {
-#	Import-Module UniversalDashboard
-#}
-
+				RequiredVersion="2.3.2"
+			} 
+		PowerShellNugetDependencies = @{
+				Name = "Oracle.ManagedDataAccess.Core"
+			},
+			@{
+				Name = "libphonenumber-csharp"
+				SkipDependencies = $true
+			}
+		CommandString = @"
 if (-Not `$Cache:EBSPowershellConfiguration ) {
 	`$Cache:EBSPowershellConfiguration = Get-TervisEBSPowershellConfiguration -Name $EnvironmentName
 }
 
 Set-EBSPowershellConfiguration -Configuration `$Cache:EBSPowershellConfiguration
 Invoke-TervisCustomerSearchDashboard
-"@ `
-	-UseTLS `
-	-Port 10000
+"@
+		Port = 10000
+	}
 
-	docker run -dit --name terviscustomer terviscustomerv1
-	docker exec -it terviscustomer pwsh
+	if (-not $AsContainer) {
+		Install-PowerShellApplicationUniversalDashboard @Parameters -UseTLS
+	} elseif ($AsContainer) {
+		Invoke-PowerShellApplicationDockerBuild @Parameters -UseTLS
+		# docker run -dit --name terviscustomer -P terviscustomerv1
+		# docker run -d --name terviscustomer -P terviscustomerv1
+		# docker exec -it terviscustomer pwsh
+		# docker run --name terviscustomer -P terviscustomerv1
+	}
 }
